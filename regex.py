8f1cab83d235fa70dfaf9368b33482628be8bafb
Brandon West <43622851+BrandWest@users.noreply.github.com>
2020-01-28 18:17:54 -0800

    Add files via upload
    Finished regex.py

--- regex.py
+++ regex.py
@@ -1,76 +1,110 @@
 #!/usr/bin/python3
-
-# Questions:
-# how long should the bin string be?
-# for the binary are we expexcted to remove the spaces if there are some in the document? The instructions are unclear.
-
-
+from pyfiglet import Figlet
+from texttable import Texttable
 import re
 import csv
 
 
-def main():
-    # create array, store the function name, string
+# TODO Currently cant do inputs like: bio,bio,bio
+# TODO Check really large stuff
+def main(location):
     yes = 0
     no = 0
-    index = 0
-    count = 0
-    i = 0
-    noCall = 0
     rules = ["student,", "password,", "username,", "email,", "previous,", "phone,", "postal,", "address,", "binary,",
-             "bio,", "\n"]
-    badChars = ["\n"]
+             "bio,"]
     stringList = list()
-
-    with open("/home/caboose/Desktop/ssd/assignment1Regex/sampleFile.csv", "r") as data:
+    # csv parser
+    with open(location, "r") as data:
         for ruleCombo in data:
             for i in range(0, len(rules)):
-                if rules[i] in ruleCombo:
-                    # print(ruleCombo)
-                    rule, info = ruleCombo.split(",")
+                if ',' in ruleCombo:
+                    rule, info = ruleCombo.split(',', 1)
+                    stringList.append(rule)
+                    stringList.append(info)
+                    break
                 else:
-                    continue
-            stringList.append(rule)
-            stringList.append(info)
-        # read whole file into array,
+                    ruleCombo.replace('\n', '')
+                    break
         # To check each of the functions that are to be called.
         for i in range(0, len(stringList)):
             if "student" in stringList[i]:
-                student(stringList[i + 1])
+                result = student(stringList[i + 1])
                 i = i + 1
+                if result == 0:
+                    yes = yes + 1
+                else:
+                    no = no + 1
             elif "password" in stringList[i]:
-                password(stringList[i + 1])
+                result = password(stringList[i + 1])
                 i = i + 1
+                
+                if result == 0:
+                    yes = yes + 1
+                else:
+                    no = no + 1
             elif "username" in stringList[i]:
-                username(stringList[i + 1])
+                result = username(stringList[i + 1])
                 i = i + 1
+                
+                if result == 0:
+                    yes = yes + 1
+                else:
+                    no = no + 1
             elif "email" in stringList[i]:
-                email(stringList[i + 1])
+                result = email(stringList[i + 1])
                 i = i + 1
+                if result == 0:
+                    yes = yes + 1
+                else:
+                    no = no + 1
             elif "previous" in stringList[i]:
-                previous(stringList[i + 1], stringList[i - 1])
+                result = previous(stringList[i + 1], stringList[i - 1])
                 i = i + 1
+                if result == 0:
+                    yes = yes + 1
+                else:
+                    no = no + 1
             elif "phone" in stringList[i]:
-                phone(stringList[i + 1])
+                result = phone(stringList[i + 1])
                 i = i + 1
+                if result == 0:
+                    yes = yes + 1
+                else:
+                    no = no + 1
             elif "postal" in stringList[i]:
-                postal(stringList[i + 1])
+                result = postal(stringList[i + 1])
                 i = i + 1
+                if result == 0:
+                    yes = yes + 1
+                else:
+                    no = no + 1
             elif "address" in stringList[i]:
-                address(stringList[i + 1])
+                result = address(stringList[i + 1])
                 i = i + 1
+                if result == 0:
+                    yes = yes + 1
+                else:
+                    no = no + 1
             elif "binary" in stringList[i]:
-                binary(stringList[i + 1])
+                result = binary(stringList[i + 1])
                 i = i + 1
+                if result == 0:
+                    yes = yes + 1
+                else:
+                    no = no + 1
             elif "bio" in stringList[i]:
-                bio(stringList[i + 1])
+                result = bio(stringList[i + 1])
                 i = i + 1
+                if result == 0:
+                    yes = yes + 1
+                else:
+                    no = no + 1
             else:
-                noCall = noCall + 1
+                continue
 
-        print(yes)
-        print(no)
-        print(noCall)
+        t = Texttable()
+        t.add_rows([["Category", "Totals"], ['Yes', yes], ['No', no]])
+        print(t.draw())
 
 
 # This can be used to verify the strings that are being shown as "right"
@@ -78,61 +112,66 @@ def main():
 def student(string):
     regex = string.strip()
     # Checks for 0-9 for 9 or [0-9]{3}_x3
-    print(re.search('([0-9]){9}|[0-9]{3} [0-9]{3} [0-9]{3}', regex))
     if re.search('([0-9]){9}|[0-9]{3} [0-9]{3} [0-9]{3}', regex):
-        print("student: " + regex + " yes")
+        print("yes")
+        return 0
     else:
-        print("student: " + regex + " no")
+        print("no")
+        return 1
 
 
 def password(string):
     regex = string.replace('\n', '')
-    print(re.search('^[ -~]{12}', regex))
-    # [ -~] does all ascii characters minimum of 12
     if re.search('^[ -~]{12}', regex):
-        print("password: " + regex + " yes")
+        print("yes")
+        return 0
     else:
-        print("password: " + regex + " no")
+        print("no")
+        return 1
 
 
 def username(string):
     regex = string.replace('\n', '')
-    print(re.search('^[a-zA-Z0-9]{3,20}', regex))
 
     if re.search('^[a-zA-Z0-9]{3,20}', regex):
-        print("username: " + regex + " yes")
+        print("yes")
+        return 0
     else:
-        print("username: " + regex + " no")
+        print("no")
+        return 1
 
 
 def email(string):
     # .net, .ca .com . org .gov .int .edu
     # lots of edge cases to look at.
     regex = string.replace('\n', '')
-
     if re.search("[a-zA-Z0-9]{3,20}@[a-zA-Z]+\\.(ca|com|org|gov|net|int|edu)$", regex):
-        print(re.search("[a-zA-Z0-9]{3,20}@[a-zA-Z]+\\.(ca|com|org|gov|net|int|edu)$", regex))
-        print("email: " + regex + " yes")
+        print("yes")
+        return 0
     else:
-        print("email: " + regex, " no")
+        print("no")
+        return 1
 
 
 def previous(string, previousString):
     # remove the leading info using regex, save it? then check both
     regex = string.replace('\n', '')
     if string == previousString:
-        print("previous: " + regex + " yes")
+        print("yes")
+        return 0
     else:
-        print("previous: " + regex, " no")
+        print("no")
+        return 1
 
 
 def phone(string):
     regex = string.replace('\n', '')
     if re.search('^(\\([0-9]{3}\\)|[0-9]{3}\\.|[0-9]{3}-|[0-9]{10})[0-9]{3}(\\.|-)[0-9]{4}', regex):
-        print(re.search('(\\([0-9]{3}\\)|[0-9]{3}\\.|[0-9]{3}-|[0-9]{10})', regex))
-        print("phone: " + regex + " Yes")
+        print("yes")
+        return 0
     else:
-        print("phone: " + regex + " no")
+        print("no")
+        return 1
 
 
 def postal(string):
@@ -140,38 +179,50 @@ def postal(string):
     regex = string.replace('\n', '')
 
     if re.search('([A-Za-z][0-9][A-Za-z] |[A-Za-z][0-9][A-Za-z])[0-9][A-Za-z][0-9]', regex):
-        print(re.search('([A-Za-z][0-9][A-Za-z] |[A-Za-z][0-9][A-Za-z])[0-9][A-Za-z][0-9]', regex))
-        print("postal: " + regex + " yes")
+        print("yes")
+        return 0
     else:
-        print("postal: " + regex, " no")
+        print("no")
+        return 1
 
 
 def address(string):
     regex = string.replace('\n', '')
     if re.search('[0-9]{2,4}( |-[0-9]{1,3}|#[0-9]{1,3})[a-zA-Z]+[A-Za-z]+(\\.|)', regex):
-        print("address: " + regex + " yes")
+        print("yes")
+        return 0
     else:
-        print("address: " + regex, " no")
+        print("no")
+        return 1
 
 
-# TODO binary numbers and questions for nick
 def binary(string):
     regex = string.replace('\n', '')
-    if re.search('[0-1]{2,}', regex):
-        print(re.search('[0-1]{2,}', regex))
-        print("binary: " + regex + " yes")
+    if re.search('[0-1]+', regex):
+        print("yes")
+        return 0
     else:
-        print("binary: " + regex, " no")
+        print("no")
+        return 1
 
 
 def bio(string):
-    regex = string.replace('\n', '')
-    if re.search('<([a-zA-Z]+)(>|| )[\\w]+</([a-zA-Z]+)>', regex):
-        print(re.search('<([a-zA-Z]+)(>|| )[\\w]+</([a-zA-Z]+)>', regex))
-        print("bio: " + regex + " yes")
+    regex = string.replace('\n', ' ')
+    if re.search('<([a-zA-Z]+)(>||[ -~]+)[ -~]*(</[a-zA-Z]+|)>', regex):
+        print("yes")
+        return 0
     else:
-        print("bio: " + regex, " no")
+        print("no")
+        return 1
 
 
 if __name__ == "__main__":
-    main()
+    custom_font = Figlet(font='jazmine')
+    print(custom_font.renderText("Regex Machine"))
+
+    location = input("Enter the file location (/path/to/file) or 1 for default: ")
+    if location == 1:
+        location = '/home/caboose/Desktop/ssd/assignment1Regex/sampleFile.csv'
+        main(location)
+    else:
+        main(location)
